ed2abb246804a14fc8e83a68dd08a190cae79dd0
ruanyf <yifeng.ruan@gmail.com>
2017-12-15 14:24:49 +0800

    feat: edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e # Exit with nonzero exit code if anything fails
 
 chmod 600 wangdoc-deploy-rsa
-scp -i wangdoc-deploy-rsa -r dist/* wangdoc@wangdoc.com:/var/www/html
+scp -i wangdoc-deploy-rsa -r dist/* wangdoc@wangdoc.com:/www/www
