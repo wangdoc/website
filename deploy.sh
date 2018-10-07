fb75394fa8b0967e98e41f8749012928a98b375b
ruanyf <yifeng.ruan@gmail.com>
2018-10-07 22:33:56 +0800

    feat: replace scp with rsync in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 set -e # Exit with nonzero exit code if anything fails
 
 chmod 600 wangdoc-deploy-rsa
-scp -i wangdoc-deploy-rsa -r dist/* "wangdoc@$host_ip":/home/wangdoc/www/www
+# scp -i wangdoc-deploy-rsa -r dist/* "wangdoc@$host_ip":/home/wangdoc/www/www
+rsync -avze "ssh -i wangdoc-deploy-rsa" --delete --progress dist/ "wangdoc@$host_ip":/home/wangdoc/www/www/
