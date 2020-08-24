7aee679ab9138910d19400010b80bae0be114b8b
ruanyf <yifeng.ruan@gmail.com>
2020-08-24 16:44:53 +0800

    refactor: edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -e # Exit with nonzero exit code if anything fails
 
 chmod 600 wangdoc-deploy-rsa
 # scp -i wangdoc-deploy-rsa -r dist/* "wangdoc@$host_ip":/home/wangdoc/www/www
-rsync -avze "ssh -i wangdoc-deploy-rsa" --delete --progress --omit-dir-times dist/ "wangdoc@$host_ip":/home/wangdoc/www/www/
+rsync -avze "ssh -i wangdoc-deploy-rsa" --delete --progress --size-only --omit-dir-times dist/ "wangdoc@$host_ip":/home/wangdoc/www/www/
